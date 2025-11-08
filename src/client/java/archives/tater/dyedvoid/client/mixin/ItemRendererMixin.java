1242890fba5139f1d363746169cf50f58a712e19
ThePotatoArchivist <josiahfu@gmail.com>
2025-11-08 01:48:34 -0800

    Fix item rendering on sinytra

--- src/client/java/archives/tater/dyedvoid/client/mixin/ItemRendererMixin.java
+++ src/client/java/archives/tater/dyedvoid/client/mixin/ItemRendererMixin.java
@@ -1,13 +1,16 @@
 package archives.tater.dyedvoid.client.mixin;
 
 import archives.tater.dyedvoid.DyedVoidClient;
+
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.item.ItemRenderer;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.item.ItemStack;
 
 @Mixin(ItemRenderer.class)
 public class ItemRendererMixin {
@@ -19,5 +22,18 @@ public class ItemRendererMixin {
     private int modifyLight(int light, @Local(argsOnly = true) ItemStack stack) {
         if (DyedVoidClient.isFlatRendered(stack)) return LightmapTextureManager.MAX_LIGHT_COORDINATE;
         return light;
+    }
+
+    @ModifyArg(
+            method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;IILnet/minecraft/client/render/model/BakedModel;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderer;getDirectItemGlintConsumer(Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/RenderLayer;ZZ)Lnet/minecraft/client/render/VertexConsumer;"),
+            index = 1
+    )
+    private static RenderLayer modifyVoidItemLayers(RenderLayer layer, @Local(argsOnly = true) ItemStack stack) {
+        if (DyedVoidClient.isPortalRendered(stack)) {
+            return RenderLayer.getEndPortal();
+        }
+        if (DyedVoidClient.isFlatRendered(stack)) return RenderLayer.getSolid();
+        return layer;
     }
 }
