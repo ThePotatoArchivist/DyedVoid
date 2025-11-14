52fce07c6e3589f7a8fadde3100c0f55e845e7ea
ThePotatoArchivist <josiahfu@gmail.com>
2025-11-14 03:51:28 -0800

    Fix crash

--- src/main/java/archives/tater/dyedvoid/mixin/EndGatewayBlockMixin.java
+++ src/main/java/archives/tater/dyedvoid/mixin/EndGatewayBlockMixin.java
@@ -1,23 +1,26 @@
 package archives.tater.dyedvoid.mixin;
 
 import archives.tater.dyedvoid.EndVoidBlock;
-import net.minecraft.block.EndGatewayBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.TeleportTarget;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.block.EndGatewayBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TeleportTarget;
+
 @Mixin(EndGatewayBlock.class)
 public class EndGatewayBlockMixin {
     @Inject(
             method = "createTeleportTarget",
-            at = @At(value = "CONSTANT", args = "classValue=net/minecraft/entity/projectile/thrown/EnderPearlEntity")
+            at = @At("RETURN")
     )
     private void craftEndVoid(ServerWorld world, Entity entity, BlockPos pos, CallbackInfoReturnable<TeleportTarget> cir) {
-        EndVoidBlock.tryCraft(entity);
+        if (cir.getReturnValue() != null)
+            EndVoidBlock.tryCraft(entity);
     }
 }
