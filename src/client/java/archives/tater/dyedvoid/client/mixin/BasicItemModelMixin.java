25e34314a524155410aec9072cfd1a38b52e3325
ThePotatoArchivist <josiahfu@gmail.com>
2025-12-10 02:03:22 -0800

    Port to 1.21.11

--- src/client/java/archives/tater/dyedvoid/client/mixin/BasicItemModelMixin.java
+++ src/client/java/archives/tater/dyedvoid/client/mixin/BasicItemModelMixin.java
@@ -1,26 +1,29 @@
 package archives.tater.dyedvoid.client.mixin;
 
 import archives.tater.dyedvoid.DyedVoidClient;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.RenderLayers;
-import net.minecraft.item.ItemStack;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(RenderLayers.class)
-public class RenderLayersMixin {
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderLayers;
+import net.minecraft.client.render.item.model.BasicItemModel;
+import net.minecraft.item.ItemStack;
+
+@Mixin(BasicItemModel.class)
+public class BasicItemModelMixin {
     @Inject(
-            method = "getItemLayer",
+            method = "method_76557",
             at = @At("HEAD"),
             cancellable = true
     )
     private static void modifyVoidItemLayers(ItemStack stack, CallbackInfoReturnable<RenderLayer> cir) {
         if (DyedVoidClient.isPortalRendered(stack)) {
-            cir.setReturnValue(RenderLayer.getEndPortal());
+            cir.setReturnValue(RenderLayers.endPortal());
             return;
         }
-        if (DyedVoidClient.isFlatRendered(stack)) cir.setReturnValue(RenderLayer.getSolid());
+        if (DyedVoidClient.isFlatRendered(stack)) cir.setReturnValue(RenderLayers.solid());
     }
 }
