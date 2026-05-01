e96006c1e0bb176a557115d650a0320e14fb22e0
ThePotatoArchivist <josiahfu@gmail.com>
2026-05-01 01:55:46 -0700

    Fix nogravity items

--- src/main/java/archives/tater/dyedvoid/mixin/ItemEntityMixin.java
+++ src/main/java/archives/tater/dyedvoid/mixin/ItemEntityMixin.java
@@ -1,15 +1,17 @@
 package archives.tater.dyedvoid.mixin;
 
 import archives.tater.dyedvoid.registry.DyedVoidItems;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends Entity {
@@ -22,8 +24,17 @@ public abstract class ItemEntityMixin extends Entity {
 			method = "<init>(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;DDD)V",
 			at = @At("TAIL")
 	)
-	private void checkNoGravityTag(Level world, double x, double y, double z, ItemStack stack, double velocityX, double velocityY, double velocityZ, CallbackInfo ci) {
-		if (stack.is(DyedVoidItems.NO_GRAVITY_TAG))
+	private void checkNoGravityTag(Level level, double x, double y, double z, ItemStack itemStack, double deltaX, double deltaY, double deltaZ, CallbackInfo ci) {
+		if (itemStack.is(DyedVoidItems.NO_GRAVITY_TAG))
+			setNoGravity(true);
+	}
+
+	@Inject(
+			method = "<init>(Lnet/minecraft/world/level/Level;DDDLnet/minecraft/world/item/ItemStack;)V",
+			at = @At("TAIL")
+	)
+	private void checkNoGravityTag(Level level, double x, double y, double z, ItemStack itemStack, CallbackInfo ci) {
+		if (itemStack.is(DyedVoidItems.NO_GRAVITY_TAG))
 			setNoGravity(true);
 	}
 }
